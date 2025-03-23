f158396337a356ef2fa9ff8663f736b9f8936f5f
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-03-23 20:27:42 +0300

    Adds Fetch

--- src/index.js
+++ src/index.js
@@ -8,4 +8,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const voteForm = document.getElementById("votes-form");
     const resetButton = document.getElementById("reset-btn");
     const newCharacterForm = document.getElementById("character-form");
+
+    let  currentCharacter = null;
+
+    // Fetch and display in character bar
+    function fetchCharacters() {
+        fetch("http://localhost:3000/characters")
+            .then(response => response.json())
+            .then(data => {
+                characterBar.innerHTML = "";
+                data.forEach(character => createCharacterSpan(character));
+            })
+            .catch(error => console.error("Error fetching characters:", error));
+    }
 })  
