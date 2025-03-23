5e6c5c9c746af5be9fefdf7a133bc71e1c960e58
Alex Dunstan Kiiru Mureithi <alexkiiru7@gmail.com>
2025-03-23 20:17:22 +0300

    declares Variables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const characterBar = document.getElementById("character-bar");
+    const detailedInfo = document.getElementById("detailed-info");
+    const characterName =  document.getElementById("name");
+    const characterImage = document.getElementById("image");
+    const characterVotes = document.getElementById("vote-count");
+    const voteForm = document.getElementById("votes-form");
+    const resetButton = document.getElementById("reset-btn");
+    const newCharacterForm = document.getElementById("character-form");
+})  
